470b85927d26a33c23ceedcc706f72a66b270263
AZDOUD Mohamed <mohamed.azd49@gmail.com>
2024-01-16 19:21:43 +0100

    id julien

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const summoners = [
   new Summoner("Thv9OHEhk7foYhwnwIlZSKMs-TVXOOliR-XIncJ3rQkUOuX7", "330746797842759681"),
   new Summoner("nOOKlG4MEE6W-W2GM8CHl0Ruv9SVEKiUTPfnzn32lm1lLGZr", "410796897398423563"),
   new Summoner("pjc66RDlPA8f0IpLsJVuxNvOYS9276AWba92sHJBU828TgU", "328484167119536128"),
-  new Summoner("DFST_qORn2PSAjSPHHi35-zxqfiGaDnjVw4JJadiT8goEAmIJfeiv2Nc_g", "571030411346706446"),
+  new Summoner("nocALPmf5wEFXbDfUsbd6LGagreBdsrP64VeAMA1ph2hWDc", "571030411346706446"),
 ];
 
 client.once("ready", async () => {
